0211a57fd1b8ed2833ffc37d578d4f9ee0f8bbdd
Jakub Rumpel <jakub@rumpel.pl>
2020-06-26 00:54:23 +0200

    added correct connection strings object in appsettings

---
+++ StoredProceduresBackup/ProcedureObject.cs
@@ -0,0 +1,8 @@
+namespace StoredProceduresBackup
+{
+    public class ProcedureObject
+    {
+        public string SchemaName { get; set; }
+        public string ProcedureName { get; set; }
+    }
+}
