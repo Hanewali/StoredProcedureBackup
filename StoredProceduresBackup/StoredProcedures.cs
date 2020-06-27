83906b52fd55ce282f75e2660f4fc6e69309693c
Jakub Rumpel <jakub@life-coder.pl>
2020-06-27 20:44:04 +0200

    Added file and git saving functions to StoredProcedures.cs

--- StoredProceduresBackup/StoredProcedures.cs
+++ StoredProceduresBackup/StoredProcedures.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
+using System.Management.Automation;
 using Microsoft.SqlServer.Management.Smo;
 
 namespace StoredProceduresBackup
 {
     public class StoredProcedures
     {
-        public List<StoredProcedure> Procedures { get; set; }
+        public List<StoredProcedure> Procedures { get; }
+        private string _directory { get; set; }
 
         public StoredProcedures()
         {
             Procedures = new List<StoredProcedure>();
+            _directory = Directory.GetParent(AppContext.BaseDirectory).FullName;
         }
 
         public void Save()
@@ -22,13 +27,25 @@ namespace StoredProceduresBackup
 
         private void SaveToFiles()
         {
-            Console.WriteLine(Directory.GetParent(AppContext.BaseDirectory).FullName);
+            Console.WriteLine(Directory.GetParent(AppContext.BaseDirectory));
+            foreach (var procedure in Procedures)
+            {
+                procedure.Refresh();
+                var content = procedure.TextHeader + procedure.TextBody;
+                var path = $"{_directory}/StoredProcedures/{procedure.Name}.sql";
+                File.WriteAllText(path, content);
+            }
         }
 
         private void SaveToGit()
         {
-            return;
+            using (PowerShell powerShell = PowerShell.Create())
+            {
+                powerShell.AddScript($"cd {_directory}/StoredProcedures/");
+                powerShell.AddScript(@"git add *");
+                powerShell.AddScript($"git commit -m 'Timestamp {DateTime.Now.ToShortDateString()}'");
+                Collection<PSObject> results = powerShell.Invoke();
+            }
         }
-
     }
 }
