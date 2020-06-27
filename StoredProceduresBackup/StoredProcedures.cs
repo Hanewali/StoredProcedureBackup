440dfbab5dd3ddbcef2691d58d129a522b3ae3e2
Jakub Rumpel <jakub@life-coder.pl>
2020-06-27 20:06:43 +0200

    moved procedures to separate class with new methods

--- StoredProceduresBackup/StoredProcedures.cs
+++ StoredProceduresBackup/StoredProcedures.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.SqlServer.Management.Smo;
+
 namespace StoredProceduresBackup
 {
-    public class Procedures
+    public class StoredProcedures
     {
-        
+        public List<StoredProcedure> Procedures { get; set; }
+
+        public StoredProcedures()
+        {
+            Procedures = new List<StoredProcedure>();
+        }
+
+        public void Save()
+        {
+            SaveToFiles();
+            SaveToGit();
+        }
+
+        private void SaveToFiles()
+        {
+            Console.WriteLine(Directory.GetParent(AppContext.BaseDirectory).FullName);
+        }
+
+        private void SaveToGit()
+        {
+            return;
+        }
+
     }
 }
