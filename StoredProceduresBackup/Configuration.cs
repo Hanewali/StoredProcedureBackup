9ba9f089d964eb418a82e01874bcef13bf5b0343
Jakub Rumpel <jakub@life-coder.pl>
2020-06-27 19:46:51 +0200

    Moved configuration actions to Configuration.cs

--- StoredProceduresBackup/Configuration.cs
+++ StoredProceduresBackup/Configuration.cs
@@ -1,7 +1,44 @@
+using System;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace StoredProceduresBackup
 {
     public class Configuration
     {
+        private IConfigurationRoot _configuration;
+
+        public SqlConnection Connection;
+
+        public Configuration()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            Connection.Open();
+        }
+
+        private void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            serviceCollection.AddSingleton(_configuration);
+
+            Connection = new SqlConnection(GetConnectionString());
+        }
         
+        private string GetConnectionString()
+        {
+            return _configuration.GetConnectionString("MyConnection");
+        }
+        
+        public string GetProceduresQuery()
+        {
+            return File.ReadAllText(Directory.GetParent(AppContext.BaseDirectory).FullName + "/GetProcedures.sql");
+        }
     }
 }
