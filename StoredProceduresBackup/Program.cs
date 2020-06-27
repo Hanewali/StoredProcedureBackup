55a2c9e9df41dc5c8373317913bb21a78f6fcafb
Jakub Rumpel <jakub@life-coder.pl>
2020-06-27 20:07:19 +0200

    Applied new logic to Program.cs

--- StoredProceduresBackup/Program.cs
+++ StoredProceduresBackup/Program.cs
@@ -13,39 +13,39 @@ namespace StoredProceduresBackup
     public class Program
     {
         private static List<ProcedureObject> _procedureObjects;
-        private static List<StoredProcedure> _procedures;
         private static Configuration _configuration;
         private static string _proceduresQuery;
         private static SqlCommand _command;
         private static ServerConnection _serverConnection;
         private static Server _server;
         private static Database _database;
-
-        private void Prepare()
+        private static StoredProcedures _storedProcedures;
+        
+        private static void Prepare()
         {
             _procedureObjects = new List<ProcedureObject>();
-            _procedures = new List<StoredProcedure>();
             _configuration = new Configuration();
             _proceduresQuery = _configuration.GetProceduresQuery();
             _command = new SqlCommand(_proceduresQuery, _configuration.Connection);
             _serverConnection = new ServerConnection(_configuration.Connection);
             _server = new Server(_serverConnection);
             _database = _server.Databases[_configuration.Connection.Database];
+            _storedProcedures = new StoredProcedures();
         }
         
         static void Main(string[] args)
         {
+            Prepare();
             ReadProceduresNames(_command);
             GetProcedures();
-            
-            
+            _storedProcedures.Save();
         }
 
         private static void GetProcedures()
         {
             foreach (var procedureObject in _procedureObjects)
             {
-                _procedures.Add(new StoredProcedure(_database, procedureObject.ProcedureName, procedureObject.SchemaName));
+                _storedProcedures.Procedures.Add(new StoredProcedure(_database, procedureObject.ProcedureName, procedureObject.SchemaName));
             }
         }
         
@@ -63,6 +63,5 @@ namespace StoredProceduresBackup
 
             reader.Close();
         }
-        
     }
 }
