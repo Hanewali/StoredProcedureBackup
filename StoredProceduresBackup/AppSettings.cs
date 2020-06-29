3a39411d29f74f5aaffbdf4d5bf010c1afc86dab
Jakub Rumpel <jakub@rumpel.pl>
2020-06-29 20:22:19 +0200

    Refactor of AppSettings class

--- StoredProceduresBackup/AppSettings.cs
+++ StoredProceduresBackup/AppSettings.cs
@@ -4,27 +4,7 @@ namespace StoredProceduresBackup
 {
     public class AppSettings
     {
-        // public Logging Logging { get; set; }
         public Dictionary<string, Dictionary<string,string>> Logging { get; set; }
         public Dictionary<string, string> ConnectionStrings { get; set; }
-        
-    }
-
-    public class Logging
-    {
-        public List<Dictionary<string,string>> LogLevel { get; set; }
-        // public LogLevel LogLevel { get; set; }
-    }
-
-    public class LogLevel
-    {
-        public string Default { get; set; }
-        public string System { get; set; }
-        public string Information { get; set; }
-    }
-
-    class ConnectionStrings
-    {
-        
     }
 }
