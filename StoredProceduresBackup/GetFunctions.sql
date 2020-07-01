cae014a5e97ebb444909a03836b7442eab2b2afe
Jakub Rumpel <jakub.rumpel@veloce.pl>
2020-07-01 13:27:07 +0200

    Added sql script for downloading functions

---
+++ StoredProceduresBackup/GetFunctions.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+     name AS 'Function Name',
+     SCHEMA_NAME(schema_id) AS 'Schema',
+     type_desc AS 'Function Type', 
+     create_date AS 'Created Date'
+FROM
+     sys.objects
+WHERE
+     type in ('FN', 'IF', 'FN', 'AF', 'FS', 'FT');
