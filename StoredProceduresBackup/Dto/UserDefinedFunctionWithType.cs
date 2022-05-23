8480892b0cef2ab535e4453167b0f36c065e03a6
Jakub Rumpel <jakub@life-coder.pl>
2022-05-23 19:31:11 +0200

    Added extension object for function and functiontype

---
+++ StoredProceduresBackup/Dto/UserDefinedFunctionWithType.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.SqlServer.Management.Smo;
+
+namespace StoredProceduresBackup.Dto
+{
+    public class UserDefinedFunctionWithType
+    {
+        public UserDefinedFunction Function { get; set; }
+        public UserDefinedFunctionType UserDefinedFunctionType { get; set; }
+
+        public UserDefinedFunctionWithType(UserDefinedFunction function, UserDefinedFunctionType type)
+        {
+            UserDefinedFunctionType = type;
+            Function = function;
+        }
+    }
+}
